3eddd3dd74843fa68e89e7729f2e0c6831339baf
N195T <knight.thapthai@gmail.com>
2025-10-16 17:14:23 +0700

    Refactor authOptions declaration in NextAuth configuration
    - Changed the export of authOptions to a const declaration in the NextAuth configuration file for improved clarity and consistency in the code structure.

--- frontend/src/app/api/auth/[...nextauth]/route.ts
+++ frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 import { authApi } from "@/lib/api";
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   providers: [
     // Google Provider (Firebase)
     GoogleProvider({
